a3dad2fb383dc4e1ec23e7a3e9374022a7a226c9
Nasser Ghaleb <63391304+NassCode@users.noreply.github.com>
2024-08-03 14:47:52 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PersistentDrawerRight from './components/Drawer';
+
+function App() {
+  return (
+    <div className="App">
+      <PersistentDrawerRight />
+    </div>
+  );
+}
+
+export default App;
